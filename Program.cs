7651dc0f4cbc1778cd0a20814e4221dbf9ce2dd2
HUAWEI <HUAWEI@LAPTOP-H6PN7ITJ>
2024-04-19 14:17:24 +0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using Model;
+using BusinessLayer;
+
+namespace RestaurantMenu
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What meal do you want to see?");
+            Console.WriteLine("Enter Meal number: ");
+            string meal = Console.ReadLine();
+            DisplayMealNumInfo(meal);
+        }
+
+        public static void DisplayMealNumInfo( string stringmeal)
+        {
+            Services businessServices = new Services();
+            List<Menu> foundMenus = businessServices.GetMenusByMealNum(stringmeal);
+
+            if(foundMenus.Count > 0)
+            {
+                Console.WriteLine($"Found meal for {stringmeal}(s):");
+                for (int i = 0; i < foundMenus.Count; i++)
+                {
+                    Console.WriteLine($"Description: {foundMenus[i].Description}");
+                    Console.WriteLine($"Meal: {foundMenus[i].Meal}");
+                    Console.WriteLine($"Price: {foundMenus[i].Price}");
+                    Console.WriteLine($"Drinks: {foundMenus[i].Drinks}");
+                }
+            }
+            else
+            {
+                Console.WriteLine("No Meal Information for the specified Meal Number");
+            }
+        }
+    }
+}
